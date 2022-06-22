f8ea25a7201fd30e016728143349f2c1ee19b74a
Michael Maurer <3191110276@users.noreply.github.com>
2022-06-22 23:17:09 +0200

    trying otel

--- images/catalog/index.js
+++ images/catalog/index.js
@@ -1,27 +1,29 @@
 const http = require('http');
 const express = require('express');
-//const bodyParser = require('body-parser');
 const fs = require('fs');
+
 
 // Constants
 const PORT = 80;
 const HOST = '0.0.0.0';
 
+
 // App
 const app = express();
-
-
-//app.use(bodyParser.json());
 app.use(express.json());
 
 
+// Catalog Route
 app.get('/', (req, res) => {
+  
     console.log('Received request for catalog items')
 
     var data = [{
+      "id": 1,
       "name": "product1",
       "price": 44.55
     },{
+      "id": 2,
       "name": "product2",
       "price": 12.99
     }]
@@ -30,10 +32,12 @@ app.get('/', (req, res) => {
 });
 
 
+// Health Check
 app.get('/healthz', (req, res) => {
   res.send('ok');
 });
 
 
+// Start Server
 app.listen(PORT, HOST);
 console.log('Running on http://${HOST}:${PORT}');
