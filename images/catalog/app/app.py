a239d863a36b737beb5355d2bec7e947884044aa
Michael Maurer <3191110276@users.noreply.github.com>
2022-06-26 01:44:23 +0200

    Changed to Python

---
+++ images/catalog/app/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+import json
+import requests
+import logging
+import sys
+
+
+app = Flask(__name__)
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    stream=sys.stdout,
+    level=logging.INFO
+)
+
+
+def load_env_file(name):
+    with open("/etc/customization/{}".format(name), "r") as f:
+        return f.read()
+
+
+@app.route("/", methods = ['GET'])
+def get_catalog():
+    catalog = [{
+        'id': 1,
+        'name': 'product1',
+        'price': '15.55'
+    },{
+        'id': 2,
+        'name': 'product2',
+        'price': '12.89'
+    }]
+
+    return json.dumps(catalog)
+
+
+# ROUTE FOR HEALTH CHECK
+@app.route("/healthz")
+def health():
+    return "ok"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ images/catalog/gunicorn_config.py
@@ -0,0 +1,4 @@
+bind = "0.0.0.0:80"
+workers = 8
+threads = 8
+timeout = 120
